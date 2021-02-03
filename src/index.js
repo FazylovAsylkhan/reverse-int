8fc111f524e824a80b9ec48c87cf369a2bcb1ab7
Fazylov Asylkhan <fazylov.asylkhan@gmail.com>
2021-02-03 21:47:24 +0500

    The task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result;
+
+    if (n < 0) {
+        result = +String(n * -1).split('').reverse().join('');
+    } else {
+        result = +String(n).split('').reverse().join('');
+    }
+    
+    return result;
 }
+
